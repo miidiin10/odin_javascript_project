8f174bc543aaa40982f0d3562fb5db8bc4352a9c
hameed <hmd.dtl@gmail.com>
2021-12-01 11:32:41 +0100

    odin_javascript_project

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,7 +1,90 @@
-function computerPlay() {
-    const array = ['Rock', 'Paper', 'Scissors'];
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById('user-score');
+const computerScore_span = document.getElementById('computer-score');
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p")
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
 
-    return random(array);
-
-    
+function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
 }
+
+function convertToWord(letter) {
+    if (letter === "r")  return "Rock";
+    if (letter === "p")  return "Paper";
+    return "Scissors";
+}
+
+function win(userChoice, computerChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallCompWord = "comp".fontsize(3).sub();
+    const winWord = "You won".fontcolor("purple");
+    const userChoice_div = document.getElementById(userChoice);
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats  ${convertToWord(computerChoice)}${smallCompWord}, ${winWord}!`;
+    userChoice_div.classList.add('purple-glow');
+    setTimeout(() => userChoice_div.classList.remove('purple-glow'), 300); // Es6 function writing
+}
+
+function lose(userChoice, computerChoice) {
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallCompWord = "comp".fontsize(3).sub();
+    const lostWord = "You lost".fontcolor("red");
+    const userChoice_div = document.getElementById(userChoice);  
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to ${convertToWord(computerChoice)}${smallCompWord}, ${lostWord}!`;
+    userChoice_div.classList.add('red-glow');
+    setTimeout(function() { userChoice_div.classList.remove('red-glow') }, 300);  // Es5 function
+}
+
+function draw(userChoice, computerChoice) {
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallCompWord = "comp".fontsize(3).sub();
+    const drawWord = "It's a Draw".fontcolor("green");
+    const userChoice_div = document.getElementById(userChoice);
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equals  ${convertToWord(computerChoice)}${smallCompWord}, ${drawWord}!`;
+    userChoice_div.classList.add('green-glow');
+    setTimeout(function() { userChoice_div.classList.remove('green-glow') }, 300);  // Es5 function
+}
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win(userChoice, computerChoice);
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            lose(userChoice, computerChoice);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw(userChoice, computerChoice);
+
+    }
+}
+
+function main() {
+    rock_div.addEventListener('click', () => game("r")); // Es6 way of writing functions
+    paper_div.addEventListener('click', function() {      // Es5 function
+        game("p");
+    })
+    scissors_div.addEventListener('click', function() {     // Es5 function
+        game("s");
+    })
+}
+
+main();
