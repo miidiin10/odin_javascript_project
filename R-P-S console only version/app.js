79c104488fbe4e1a21a2e73c1fa6f9110c76379a
hameed <hmd.dtl@gmail.com>
2021-12-02 13:15:28 +0100

    odin_javascript_project

--- R-P-S console only version/app.js
+++ R-P-S console only version/app.js
@@ -3,4 +3,34 @@ function computerPlay() {
     let randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 } 
-console.log(computerPlay());
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = '';
+let playerSelection = prompt('Rock, Paper or Scissors?').toLowerCase();
+
+function playRound(playerSelection, computerSelection) {
+   if(
+       (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+       (playerSelection === 'Scissors' && computerSelection === 'Paper') ||
+       (playerSelection === 'Paper' && computerSelection === 'Rock')
+     ) {
+         playerScore++;
+         return "You Win!"
+     }
+
+     if(
+        (computerSelection === 'Rock' && playerSelection === 'Scissors') ||
+        (computerSelection === 'Scissors' && playerSelection === 'Paper') ||
+        (computerSelection === 'Paper' && playerSelection === 'Rock')
+      ) {
+          playerScore++;
+          return "You Lose!"
+      }
+    
+      if (playerSelection === computerSelection) {
+          return "It's a Tie"
+      }
+
+      playerSelection = "rock";
+      computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
